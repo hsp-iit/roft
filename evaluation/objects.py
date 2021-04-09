8d4e1ccb35931e62e4cb4c3318bf9643b287f3ad
Nicola Piga <nicolapiga@gmail.com>
2021-04-09 02:09:57 +0200

    Implement fake class with object classes names

---
+++ evaluation/objects.py
@@ -0,0 +1,23 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+class Objects():
+
+    def __init__(self):
+        """Constructor"""
+
+        self.objects =\
+        {
+            '003_cracker_box',
+            '004_sugar_box',
+            '005_tomato_soup_can',
+            '006_mustard_bottle',
+            '009_gelatin_box',
+            '010_potted_meat_can'
+        }
