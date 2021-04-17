ed75422e89e782632cf4d74ca67be8a7400f0c1c
Nicola Piga <nicolapiga@gmail.com>
2021-04-17 15:15:15 +0200

    Add missing license to files

--- tools/dope/inference.py
+++ tools/dope/inference.py
@@ -1,3 +1,12 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
 import argparse
 import cv2
 import json
--- tools/dope/process_ho3d.sh
+++ tools/dope/process_ho3d.sh
@@ -1,3 +1,12 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
 HO3D_PATH=?
 DOPE_PATH=?
 DOPE_VENV_PATH=?
