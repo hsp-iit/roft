0d7647c1347cebc7fd8e16e57752772cd84a17f3
Nicola Piga <nicolapiga@gmail.com>
2021-04-18 10:02:20 +0200

    Add script for NVOF-aided optical flow for HO-3D dataset

---
+++ tools/nvof/dumper/process_ho3d.sh
@@ -0,0 +1,16 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+HO3D_PATH=/home/icub/dataset/HO3D/
+
+for SEQ_NAME in `ls $HO3D_PATH`; do
+    OUTPUT_PATH=$HO3D_PATH/$SEQ_NAME/optical_flow/nvof
+    mkdir -p $OUTPUT_PATH
+    ./build/bin/6d-of-tracking-of-dumper $HO3D_PATH/$SEQ_NAME txt png 0 0 640 480 $OUTPUT_PATH
+done
