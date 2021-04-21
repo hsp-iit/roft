d6a77ed662b52f0c23225e2a22b7e1ae0bad63f0
Nicola Piga <nicolapiga@gmail.com>
2021-04-21 11:26:03 +0200

    Add scripts to run on HO-3D using ground truth poses and masks

---
+++ test/test_full_gt_mask_gt_pose_all_ho3d.sh
@@ -0,0 +1,18 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+HO3D_PATH=`cat ./config/ho3d_location`
+
+for object_name in `cat config/classes_ho3d.txt`
+do
+    for sequence_name in `ls -d $HO3D_PATH/$object_name*`; do
+        sequence_number=`echo ${sequence_name##*_}`
+        bash test/test_full_gt_mask_gt_pose_ho3d.sh $object_name $sequence_number
+    done
+done
---
+++ test/test_full_gt_mask_gt_pose_ho3d.sh
@@ -0,0 +1,21 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+OBJECT_NAME=$1
+SEQ_NAME=$2
+GT_MASK="true"
+GT_OF="false"
+GT_POSE="true"
+NO_OUT_REJ="false"
+NO_POSE_SYNC="false"
+NO_FLOW_AID="false"
+ONLY_POSE="false"
+ONLY_VEL="false"
+
+bash test/test_ho3d.sh $OBJECT_NAME $SEQ_NAME $GT_MASK $GT_OF $GT_POSE $NO_OUT_REJ $NO_POSE_SYNC $NO_FLOW_AID $ONLY_POSE $ONLY_VEL
