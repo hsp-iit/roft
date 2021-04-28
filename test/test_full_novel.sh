b3546426554dd1821324de4c4619e66bc2d1e83a
Nicola Piga <nicolapiga@gmail.com>
2021-04-28 14:59:55 +0200

    Add support for experiments w/o poses or velocities

---
+++ test/test_full_novel.sh
@@ -0,0 +1,20 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+OBJECT_NAME=$1
+GT_MASK="false"
+GT_OF="false"
+GT_POSE="false"
+NO_OUT_REJ="false"
+NO_POSE_SYNC="false"
+NO_FLOW_AID="false"
+NO_POSE="false"
+NO_VEL="true"
+
+bash test/test.sh $OBJECT_NAME $GT_MASK $GT_OF $GT_POSE $NO_OUT_REJ $NO_POSE_SYNC $NO_FLOW_AID $NO_POSE $NO_VEL
---
+++ test/test_full_novel_all.sh
@@ -0,0 +1,13 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+for object_name in `cat config/classes.txt`
+do
+    bash test/test_full_novel.sh $object_name
+done
