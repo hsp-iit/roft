e3e97674c6f9358b73f6ea66efb01b97ff1b641a
Nicola Piga <nicolapiga@gmail.com>
2021-05-10 23:38:56 +0200

    Add shotcut project for rendering ycbv_real sequences

---
+++ tools/object_renderer/shotcut/process_ycbvr_real.sh
@@ -0,0 +1,18 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+REPOSITORY_PATH=`cat config/this_location`
+mkdir -p tools/object_renderer/shotcut/tmp
+
+for object_name in 003_cracker_box 006_mustard_bottle; do
+    for name in dope_real poserbpf se3tracknet ours; do
+        ln -sfT $REPOSITORY_PATH/evaluation_output/exp_ycbvr_real/$name/$object_name/sequence_0/output.mp4 $REPOSITORY_PATH/tools/object_renderer/shotcut/tmp/$name.mp4;
+    done
+    shotcut tools/object_renderer/shotcut/project.mlt
+done
