727ed41342fe807b980c701dd33598ca8510406e
Nicola Piga <nicolapiga@gmail.com>
2021-05-12 14:05:35 +0200

    Add standalone experiments for se3tracknet without re-initialization

--- evaluation/experiments.py
+++ evaluation/experiments.py
@@ -25,6 +25,8 @@ class Experiments():
         self.add_algorithm_to_experiment('exp_ycbvs_real_50', 'poserbpf', particles = 50, fps = 15, init_from = 'dope', reinit = False, reinit_from = '', dataset = 'ycbv_synthetic')
         self.add_algorithm_to_experiment('exp_ycbvs_real_50', 'se3tracknet', init_from = 'dope', reinit = True, reinit_from = 'dope', reinit_fps = 5, dataset = 'ycbv_synthetic')
 
+        self.add_algorithm_to_experiment('exp_ycbvs_se3_plain', 'se3tracknet', init_from = 'dope', reinit = False, reinit_from = '', dataset = 'ycbv_synthetic')
+
         self.add_algorithm_to_experiment('exp_ycbvs_ablation', 'dope', label = 'dope_ideal', dataset = 'ycbv_synthetic', simulate_inference = False)
         self.add_algorithm_to_experiment('exp_ycbvs_ablation', 'ours', label = 'ours_gt', masks_set = 'gt', of_set = 'nvof_1_slow', pose_set = 'gt', dataset = 'ycbv_synthetic')
         self.add_algorithm_to_experiment('exp_ycbvs_ablation', 'ours', label = 'ours_gt_pose', masks_set = 'mrcnn_ycbv_bop_pbr', of_set = 'nvof_1_slow', pose_set = 'gt', dataset = 'ycbv_synthetic')
@@ -47,6 +49,8 @@ class Experiments():
 
         # HO-3D
         self.add_algorithm_to_experiment('exp_ho3d', 'ours', masks_set = 'mrcnn_ycbv_bop_pbr', of_set = 'nvof_2_slow', pose_set = 'dope', dataset = 'ho3d')
+
+        self.add_algorithm_to_experiment('exp_ho3d_se3_plain', 'se3tracknet', init_from = 'dope', reinit = False, reinit_from = '', dataset = 'ho3d')
 
         self.add_algorithm_to_experiment('exp_ho3d_real', 'ours', masks_set = 'mrcnn_ycbv_bop_pbr', of_set = 'nvof_2_slow', pose_set = 'dope', dataset = 'ho3d')
         self.add_algorithm_to_experiment('exp_ho3d_real', 'poserbpf', particles = 200, fps = 7, init_from = 'dope', reinit = False, reinit_from = '', dataset = 'ho3d')
