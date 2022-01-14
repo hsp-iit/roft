452cbf0d59f86665e9c434497727c9b7f9dee2c4
Nicola Piga <nicolapiga@gmail.com>
2022-01-14 22:58:27 +0000

    Add missing license header

--- tools/dataset/conversion/conversion.py
+++ tools/dataset/conversion/conversion.py
@@ -1,3 +1,12 @@
+#===============================================================================
+#
+# Copyright (C) 2022 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
 import os
 import numpy as np
 from scipy import io
