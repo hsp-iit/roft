0bd9dcb39a103284e308990cc36c3e8947d11b7b
Nicola Piga <nicolapiga@gmail.com>
2022-01-14 23:05:17 +0000

    Add missing license header

--- tools/detectron2/generate_ycbv_bop_pbr_dataset_description.sh
+++ tools/detectron2/generate_ycbv_bop_pbr_dataset_description.sh
@@ -1,3 +1,12 @@
+#===============================================================================
+#
+# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
 YCBV_BOP_PBR_PATH='./dataset/ycbv_bop_pbr'
 DETECTRON2_VENV_PATH=`cat ./config/detectron2_venv_location`
 
