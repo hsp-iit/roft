3b6a46647f83f09f5d03e7711f0f4a13c030438d
Nicola Piga <nicolapiga@gmail.com>
2022-01-15 10:40:22 +0000

    Simplify fast-ycb dataset path

--- tools/nvof/renderer/process_ycbv_real.sh
+++ tools/nvof/renderer/process_ycbv_real.sh
@@ -10,7 +10,7 @@
 YCBVSYN_PATH='./dataset/fast-ycb'
 
 for OBJECT_NAME in 003_cracker_box_real 006_mustard_bottle_real; do
-    OUTPUT_PATH=$YCBVSYN_PATH/object_motion/$OBJECT_NAME/optical_flow/nvof_1_slow/debugging
+    OUTPUT_PATH=$YCBVSYN_PATH/$OBJECT_NAME/optical_flow/nvof_1_slow/debugging
     mkdir -p $OUTPUT_PATH
-    ./build/bin/6d-of-tracking-of-renderer $YCBVSYN_PATH/object_motion/$OBJECT_NAME nvof_1_slow 1 0 1280 720 $OUTPUT_PATH
+    ./build/bin/6d-of-tracking-of-renderer $YCBVSYN_PATH/$OBJECT_NAME nvof_1_slow 1 0 1280 720 $OUTPUT_PATH
 done
--- tools/nvof/renderer/process_ycbv_synthetic.sh
+++ tools/nvof/renderer/process_ycbv_synthetic.sh
@@ -10,7 +10,7 @@
 YCBVSYN_PATH='./dataset/fast-ycb'
 
 for OBJECT_NAME in 003_cracker_box 004_sugar_box 005_tomato_soup_can 006_mustard_bottle 009_gelatin_box 010_potted_meat_can; do
-    OUTPUT_PATH=$YCBVSYN_PATH/object_motion/$OBJECT_NAME/optical_flow/nvof_1_slow/debugging
+    OUTPUT_PATH=$YCBVSYN_PATH/$OBJECT_NAME/optical_flow/nvof_1_slow/debugging
     mkdir -p $OUTPUT_PATH
-    ./build/bin/6d-of-tracking-of-renderer $YCBVSYN_PATH/object_motion/$OBJECT_NAME nvof 1 0 1280 720 $OUTPUT_PATH
+    ./build/bin/6d-of-tracking-of-renderer $YCBVSYN_PATH/$OBJECT_NAME nvof 1 0 1280 720 $OUTPUT_PATH
 done
