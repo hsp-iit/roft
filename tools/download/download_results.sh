7cb374df7be5308363ea49ff9b260f072c1d05cf
Nicola Piga <nicolapiga@gmail.com>
2022-01-16 20:42:24 +0000

    Add scripts for downloading pre-evaluated results

---
+++ tools/download/download_results.sh
@@ -0,0 +1,3 @@
+wget "https://storage-dataverse.iit.it/dataverse/10.48557/NM330A/17e6493343b-c9059e98be6d?response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27results.tar.gz&response-content-type=application%2Fgzip&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220116T203054Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=admin%2F20220116%2Fdataverse%2Fs3%2Faws4_request&X-Amz-Signature=ce8839f9a9c3263004ec43a6c76e9df895eab7b6a683b766133c4a48f24740eb" -O results.tar.gz
+tar xzf results.tar.gz
+rm results.tar.gz
