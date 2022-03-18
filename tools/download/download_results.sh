d3b27d43e8e56b985906e7fde2e068b7c4ddb981
Nicola Piga <nicolapiga@gmail.com>
2022-03-18 14:29:19 +0100

    Update tools/download/download_results.sh

--- tools/download/download_results.sh
+++ tools/download/download_results.sh
@@ -1,6 +1,6 @@
 SERVER=https://dataverse.iit.it
 TOKEN=
 
-curl -L -H X-Dataverse-key:$TOKEN $SERVER/api/access/datafile/1768 -o results.tar.gz
+curl -L -H X-Dataverse-key:$TOKEN $SERVER/api/access/datafile/1926 -o results.tar.gz
 tar xzf results.tar.gz
 rm results.tar.gz
