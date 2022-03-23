a841dfbe70554ba16e2df17d510dfb09901f601e
Nicola Piga <nicolapiga@gmail.com>
2022-03-23 23:17:20 +0000

    Update script for downloading pre-evaluated results

--- tools/download/download_results.sh
+++ tools/download/download_results.sh
@@ -1,6 +1,9 @@
 SERVER=https://dataverse.iit.it
+PERSISTENT_ID=doi:10.48557/9FJW42
 TOKEN=
 
-curl -L -H X-Dataverse-key:$TOKEN $SERVER/api/access/datafile/1926 -o results.tar.gz
-tar xzf results.tar.gz
-rm results.tar.gz
+echo "Downloading results..."
+curl -L -H X-Dataverse-key:$TOKEN $SERVER/api/access/dataset/:persistentId/?persistentId=$PERSISTENT_ID -o results.zip
+unzip -qq results.zip
+rm results.zip
+rm MANIFEST.TXT
