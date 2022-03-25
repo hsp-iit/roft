cdc0271eeef6b778e9c8a1cf98aff0cde1e02d80
Nicola Piga <nicolapiga@gmail.com>
2022-03-25 13:14:22 +0100

    Remove the necessity to use the token in tools/download/download_results.sh

--- tools/download/download_results.sh
+++ tools/download/download_results.sh
@@ -1,9 +1,8 @@
 SERVER=https://dataverse.iit.it
 PERSISTENT_ID=doi:10.48557/9FJW42
-TOKEN=
 
 echo "Downloading results..."
-curl -L -H X-Dataverse-key:$TOKEN $SERVER/api/access/dataset/:persistentId/?persistentId=$PERSISTENT_ID -o results.zip
+curl -L $SERVER/api/access/dataset/:persistentId/?persistentId=$PERSISTENT_ID -o results.zip
 unzip -qq results.zip
 rm results.zip
 rm MANIFEST.TXT
